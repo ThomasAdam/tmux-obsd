962e56559162c27ca39249a4c254dacc2e948266
nicm <nicm>
2026-05-07 09:15:44 +0000

    Add Emacs-style recentre-top-bottom, GitHub issue 5053 from sinyax75 at gmail dot com.

--- key-bindings.c
+++ key-bindings.c
@@ -503,7 +503,7 @@ key_bindings_init(void)
 		"bind -Tcopy-mode C-b { send -X cursor-left }",
 		"bind -Tcopy-mode C-g { send -X clear-selection }",
 		"bind -Tcopy-mode C-k { send -X copy-pipe-end-of-line-and-cancel }",
-		"bind -Tcopy-mode C-l { send -X cursor-centre-vertical }",
+		"bind -Tcopy-mode C-l { send -X recentre-top-bottom }",
 		"bind -Tcopy-mode M-l { send -X cursor-centre-horizontal }",
 		"bind -Tcopy-mode C-n { send -X cursor-down }",
 		"bind -Tcopy-mode C-p { send -X cursor-up }",
